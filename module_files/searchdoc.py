996468b0a00dd43832dbee96fb27f2f935359527
LIULI19 <ll19960830@icloud.com>
2023-08-24 15:45:00 +0800

    修改关键词相关文档链接获取模块的入参类型与关键词提取模块出参的类型一致，均为字符串类型，列表转换在关键词相关文档链接获取模块内完成

--- module_files/searchdoc.py
+++ module_files/searchdoc.py
@@ -3,14 +3,15 @@ import requests
 
 def searchdoc(page, pagesize, keywords):
     url = "http://114.55.61.91:10087/searchDoc"
+    lst_keywords = keywords.split("、")
     data = {
         "page": page,
         "pageSize": pagesize,
-        "keywords": keywords
+        "keywords": lst_keywords
     }
     response = requests.post(url, json=data)
     return response.text
 
 
 if __name__ == '__main__':
-    print(searchdoc(1, 20, ["云服务器", "镜像"]))
+    print(searchdoc(1, 20, "云服务器、镜像"))
